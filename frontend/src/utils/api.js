4e253f6a56c6bb4a16a58d048f361ffe2cc9d31c
veereshdevadhar <devadharveeru@gmail.com>
2026-08-05 00:51:45 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,14 +1,29 @@
 import axios from 'axios';
 
-const BASE = "https://fairlens-ty48.onrender.com";
+export const BASE =
+  process.env.REACT_APP_API_URL ||
+  'https://fairlens-ty48.onrender.com';
 
-const api = axios.create({ baseURL: BASE });
+const api = axios.create({
+  baseURL: BASE,
+});
 
-export const fetchDatasets  = ()               => api.get('/datasets').then(r => r.data);
-export const loadDataset    = (id)             => api.get(`/datasets/${id}`).then(r => r.data);
-export const uploadFile     = (formData)       => api.post('/upload', formData).then(r => r.data);
-export const runAnalysis    = (data)           => api.post('/analyze', data).then(r => r.data);
-export const downloadReport = ()               => `${BASE}/report`;
+export const fetchDatasets = () =>
+  api.get('/datasets').then(r => r.data);
+
+export const loadDataset = (id) =>
+  api.get(`/datasets/${id}`).then(r => r.data);
+
+export const uploadFile = (formData) =>
+  api.post('/upload', formData).then(r => r.data);
+
+export const runAnalysis = (data) =>
+  api.post('/analyze', data).then(r => r.data);
+
+export const downloadReport = () =>
+  `${BASE}/report`;
+
+export const runAutopsy = () =>
+  api.get('/autopsy').then(r => r.data);
 
 export default api;
-export const runAutopsy = () => api.get('/autopsy').then(r => r.data);
